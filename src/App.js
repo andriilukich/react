144e6c13b97a7176fa8d3ad386f3bf50d04e55f0
Andrii <andriilukich@gmail.com>
2019-11-26 14:27:48 +0700

    deleted unnecessary modules

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import Tehnologies from './Tehnologies';
+import Footer from './Footer';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Tehnologies />
+      <Footer />
     </div>
   );
 }
